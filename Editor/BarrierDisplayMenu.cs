05783fd7d09835d7ccf4d904d1d3d941dd45659a
Jegg <pawel.saltarius@gmail.com>
2024-07-31 17:23:20 +0200

    Package Content

---
+++ Editor/BarrierDisplayMenu.cs
@@ -0,0 +1,55 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace BarrierDisplay.Editor
+{
+    public class BarrierDisplayMenu : EditorWindow
+    {
+        [MenuItem("Tools/Barrier Rendering Menu")]
+        public static void OpenMenu()
+        {
+            BarrierDisplayMenu window = GetWindow<BarrierDisplayMenu>();
+            window.titleContent = new GUIContent("Barrier Display Menu");
+        }
+
+        void CreateGUI()
+        {
+            Label header = new Label("Barrier Display Menu");
+            header.style.fontSize = 30;
+            header.style.unityFontStyleAndWeight = FontStyle.Bold;
+            header.style.alignSelf = Align.Center;
+            header.style.paddingTop = 10;
+            header.style.paddingBottom = 10;
+
+            Button showButton = new Button() { name = "showButton", text = "Show Barriers" };
+            Button hideButton = new Button() { name = "hideButton", text = "Hide Barriers" };
+            showButton.style.height = 30;
+            showButton.style.marginBottom = 10;
+            hideButton.style.height = 30;
+
+            showButton.clicked += ShowColliders;
+            hideButton.clicked += HideColliders;
+
+            rootVisualElement.Add(header);
+            rootVisualElement.Add(showButton);
+            rootVisualElement.Add(hideButton);
+        }
+
+        void ShowColliders()
+        {
+            BarrierRenderer[] clips = FindObjectsOfType<BarrierRenderer>();
+
+            foreach (BarrierRenderer clip in clips)
+                clip.Show();
+        }
+
+        void HideColliders()
+        {
+            BarrierRenderer[] clips = FindObjectsOfType<BarrierRenderer>();
+
+            foreach (BarrierRenderer clip in clips)
+                clip.Hide();
+        }
+    }
+}
---
+++ Runtime/BarrierRenderer.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace BarrierDisplay
+{
+    [DisallowMultipleComponent]
+    sealed class BarrierRenderer : MonoBehaviour
+    {
+        static List<BarrierRenderer> barriers = new List<BarrierRenderer>();
+
+        Renderer[] renderers = new Renderer[0];
+
+        private void Awake()
+        {
+            barriers.Add(this);
+            Hide();
+        }
+
+        public void Show()
+        {
+            ToggleVisibility(true);
+        }
+
+        public void Hide()
+        {
+            ToggleVisibility(false);
+        }
+
+        private void ToggleVisibility(bool visible)
+        {
+            if (renderers.Length == 0)
+                renderers = GetComponentsInChildren<Renderer>();
+
+            foreach (Renderer r in renderers)
+                r.enabled = visible;
+        }
+
+        private void OnDestroy()
+        {
+            barriers.Remove(this);
+        }
+    }
+}
